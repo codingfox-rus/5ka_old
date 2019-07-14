8fa2b0ea4bddd4f192577e49aab19c05280db14a
Oleg <olegserebryakoff@mail.ru>
2019-07-14 20:37:54 +0300

    fx

--- commands/StatController.php
+++ commands/StatController.php
@@ -10,7 +10,10 @@ use app\models\Feedback;
 
 class StatController extends Controller
 {
-
+    /**
+     * @return bool
+     * @throws \yii\db\Exception
+     */
     public function actionProcessLocation(): bool
     {
         $location = Location::findOne(['needToProcess' => 1]);
@@ -122,108 +125,6 @@ class StatController extends Controller
             $location->needToProcess = 0;
             $location->save(false);
 
-            return true;
-        }
-
-        echo 'No stat data'. PHP_EOL;
-        return false;
-    }
-
-    /**
-     * Собираем статистику по продуктами определенной локации
-     * @return bool
-     * @throws \yii\db\Exception
-     */
-    public function actionProcessLocationOld(): bool
-    {
-        $location = Location::findOne(['needToProcess' => 1]);
-
-        if ($location === null) {
-            echo 'Нет локаций для сбор статистики'. PHP_EOL;
-            return false;
-        }
-
-        $statRows = Stat::find()
-            ->select(['productId', 'data', 'nextTurnAt'])
-            ->location($location->id)
-            ->indexBy('productId')
-            ->asArray()
-            ->all();
-
-        $statProductIds = array_keys($statRows);
-
-        $discounts = Discount::find()
-            ->select([
-                'locationId',
-                'productId',
-                'regularPrice',
-                'specialPrice',
-                'dateStart',
-                'dateEnd',
-            ])
-            ->location($location->id)
-            ->andWhere([
-                'in', 'productId', $statProductIds
-            ])
-            ->asArray()
-            ->all();
-
-        $updatedProductData = [];
-
-        foreach ($discounts as $discount) {
-
-            foreach ($statRows as $stat) {
-
-                if ((int)$discount['productId'] === (int)$stat['productId']) {
-
-                    if ($discount['dateEnd'] <= $stat['nextTurnAt']) {
-                        continue;
-                    }
-
-                    $productStat = json_decode($stat['data'], true);
-
-                    $productStat[] = [
-                        'regularPrice' => $discount['regularPrice'],
-                        'specialPrice' => $discount['specialPrice'],
-                        'dateStart' => $discount['dateStart'],
-                        'dateEnd' => $discount['dateEnd'],
-                    ];
-
-                    $updatedProductData[$stat['productId']] = [
-                        'locationId' => $stat['locationId'],
-                        'productId' => $stat['productId'],
-                        'data' => json_encode($productStat, JSON_UNESCAPED_UNICODE),
-                        'nextTurnAt' => $discount['dateEnd'],
-                        'createdAt' => time(),
-                        'updatedAt' => time(),
-                    ];
-                }
-            }
-        }
-
-        if ($updatedProductData) {
-
-            $updatedProductIds = array_keys($updatedProductData);
-
-            $del = Stat::deleteAll([
-                'and',
-                ['locationId' => $location->id],
-                ['in', 'productId', $updatedProductIds]
-            ]);
-
-            echo $del .' stat rows deleted'. PHP_EOL;
-
-            $dataForInsert = array_values($updatedProductData);
-
-            $ins = Yii::$app->db->createCommand()
-                ->batchInsert(
-                    Stat::tableName(),
-                    array_keys($dataForInsert[0]),
-                    $dataForInsert
-                )->execute();
-
-            echo $ins .' stat rows inserted'. PHP_EOL;
-
             $alert = new Feedback();
             $alert->name = 'Статистика';
             $alert->email = Yii::$app->params['adminEmail'];
@@ -236,12 +137,10 @@ class StatController extends Controller
                 echo $errors . PHP_EOL;
             }
 
-        } else {
-            echo 'Нет данных для статистики'. PHP_EOL;
+            return true;
         }
 
-        return true;
+        echo 'No stat data'. PHP_EOL;
+        return false;
     }
-
-
 }
